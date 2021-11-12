9db2aee42e560590991f0ca28bf478f566806881
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2021-11-12 19:05:03 +0100

    Add all functionality for assignment

--- Program.cs
+++ Program.cs
@@ -37,10 +37,13 @@ namespace assignment3
           if (posts.Count > 0)
           {
             list.PrintPosts(posts);
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+            Console.Clear();
           }
           else
           {
-            Console.WriteLine("No posts in the guestbook yet, want to add one? \n");
+            Console.WriteLine("\nNo posts in the guestbook yet, want to add one? \n");
           }
 
           return true;
@@ -48,44 +51,97 @@ namespace assignment3
           Console.Clear();
           string authorName = "";
           string message = "";
+          Console.ForegroundColor = ConsoleColor.DarkCyan;
+          Console.WriteLine("\nPlease input your name:");
 
-          Console.WriteLine("Please input your name:\n");
           authorName = Console.ReadLine();
 
-          Console.WriteLine("Input your message:\n");
-          message = Console.ReadLine().Trim();
-
-          do
+          while (String.IsNullOrEmpty(authorName))
           {
             Console.Clear();
-            Console.WriteLine("You forgot to write your name or message down. Please try again. \n");
-          } while (String.IsNullOrEmpty(authorName) || String.IsNullOrEmpty(message));
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("You have to provide a name!\n");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            authorName = Console.ReadLine();
+          }
+
+          Console.WriteLine("\nInput your message:");
+          message = Console.ReadLine().Trim();
+
+          while (String.IsNullOrEmpty(message))
+          {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nYou have to provide a message!");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            message = Console.ReadLine().Trim();
+          }
 
           list.AddPost(posts, authorName, message);
-
-          Console.Clear();
-      
+          Console.ForegroundColor = ConsoleColor.Gray;
           return true;
         case "3":
           Console.Clear();
-          list.PrintPosts(posts);
-          Console.WriteLine("Please provide the number of the post you want to delete:");
 
-          string postIndexInput = Console.ReadLine();
-          int postIndex;
-
-          bool isANumber = int.TryParse(postIndexInput, out postIndex);
-
-          if (!isANumber)
+          if (posts.Count > 0)
           {
-            Console.Clear();
-            Console.WriteLine("Oh dear, you didn't put in a number, try again please\n");
-          } else {
-            list.DeletePost(postIndex, posts);
+            list.PrintPosts(posts);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nPlease provide the number of the post you want to delete:");
+
+            string postIndexInput = Console.ReadLine();
+            int postIndex;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            bool isANumber = int.TryParse(postIndexInput, out postIndex);
+
+            if (!isANumber)
+            {
+              Console.Clear();
+              Console.ForegroundColor = ConsoleColor.Yellow;
+              Console.WriteLine("\nOh dear, your input wasn't a number, try again please\n");
+              Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            else
+            {
+              int inputCheck = Convert.ToInt32(postIndexInput) + 1;
+
+              if (inputCheck > posts.Count)
+              {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\nOh dear, your input wasn't a valid post number, try again please\n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+              }
+              else
+              {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"Are you sure you want to remove post {postIndex}? Y/n:\n");
+                string answer = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                if (answer == "y" || answer == "Y" || answer == "Yes" || answer == "yes")
+                {
+                  list.DeletePost(postIndex, posts);
+                }
+                else
+                {
+                  Console.ForegroundColor = ConsoleColor.DarkGreen;
+                  Console.WriteLine("\nNo posts were deleted!");
+                  Console.ForegroundColor = ConsoleColor.Gray;
+                  Console.WriteLine("\nPress any key to continue");
+                  Console.ReadKey();
+                  Console.Clear();
+                }
+
+              }
+            }
+          }
+          else
+          {
+            Console.WriteLine("\nNo posts in the guestbook yet, want to add one? \n");
           }
 
           return true;
-
         case "0":
           Console.Clear();
           Console.WriteLine("\nThanks for visiting. Goodbye!\n");
@@ -94,14 +150,14 @@ namespace assignment3
 
         default:
           Console.Clear();
-          Console.WriteLine("Not a valid input. Please try again");
+          Console.WriteLine("\nNot a valid input. Please try again");
           return true;
       }
     }
     static void WelcomeText()
     {
       Console.ForegroundColor = ConsoleColor.DarkBlue;
-      Console.WriteLine("\n\n### R A S M U S ~ G U E S T B O O K! ###");
+      Console.WriteLine("\n### R A S M U S ~ G U E S T B O O K! ###");
       Console.WriteLine(" ");
       Console.WriteLine("What would you like to do?");
       Console.WriteLine(" ");
@@ -110,7 +166,7 @@ namespace assignment3
       Console.WriteLine("[3] Delete a post");
       Console.WriteLine("[0] Exit guestbook");
       Console.Write("\r\nSelect an option: ");
-      Console.ForegroundColor = ConsoleColor.Gray; 
+      Console.ForegroundColor = ConsoleColor.Gray;
     }
   }
 }
--- classes/PostList.cs
+++ classes/PostList.cs
@@ -10,6 +10,7 @@ namespace assignment3
   {
     public void PrintPosts(List<Post> list)
     {
+      Console.WriteLine("\nCurrent posts in the guestbook\n");
       for (int i = 0; i < list.Count; i++)
       {
         Console.WriteLine($"[{i}] Author: {list[i].author} : Message: {list[i].content}");
@@ -22,16 +23,35 @@ namespace assignment3
 
       list.Add(newPost); // adds item to the end of the collection
 
-      // TODO: Break this into method
-      string json = JsonConvert.SerializeObject(list, Formatting.Indented);
-      File.WriteAllText("posts.json", json, Encoding.UTF8);
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("\nPost successfully created!");
+      Console.ForegroundColor = ConsoleColor.Gray;
+
+      UpdateJson(list);
+
+      Console.WriteLine("\nPress any key to continue");
+      Console.ReadKey();
+      Console.Clear();
+
+      
     }
 
     public void DeletePost(int index, List<Post> list)
     {
-      list.RemoveAt(index);
+      list.RemoveAt(index); // Removes item on provided index
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("\nPost successfully removed!");
+      Console.ForegroundColor = ConsoleColor.Gray;
 
-      // TODO: Break this into method
+      UpdateJson(list);
+
+      Console.WriteLine("\nPress any key to continue");
+      Console.ReadKey();
+      Console.Clear();
+    }
+
+    public void UpdateJson(List<Post> list)
+    {
       string json = JsonConvert.SerializeObject(list, Formatting.Indented);
       File.WriteAllText("posts.json", json, Encoding.UTF8);
     }
